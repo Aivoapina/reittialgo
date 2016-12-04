6bf9eea8ea72b1594d1a2089d1b0e14cb233e95c
Anssi Varjonen <anssi.varjonen@student.tut.fi>
2016-12-04 14:44:55 +0200

    asd

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    if (req.method = 'POST'){
-
-    }
     res.render('index', { title: 'Express' });
 
     request('https://backend.24rent.fi/api2/car/search?startTime=2016-12-20%2020:00:00&endTime=2016-12-30%2014:00:00&km=50', function (error, res, body){
       var cars = JSON.parse(body);
-      console.log(cars[0]);
+      //console.log(cars[0]);
     });
+});
+router.post('/', function(req, res, next) {
+    console.log(req.body)
 });
 module.exports = router;
